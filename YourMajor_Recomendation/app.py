20242592ed79950833902b42fd11b4c8a3f3da7a
Ridhan Firdaus <ridhanmuhammad25@gmail.com>
2026-07-30 22:00:52 +0700

    Update app.py

--- YourMajor_Recomendation/app.py
+++ YourMajor_Recomendation/app.py
@@ -96,6 +96,7 @@ def inject_css():
             border-radius: 18px;
             object-fit: cover;
             box-shadow: 0 8px 25px rgba(0,0,0,0.35);
+            filter: brightness(80%)
         }
         .section-title {
             font-size: 22px;
