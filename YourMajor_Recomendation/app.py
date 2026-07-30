d9c67437c62abf0eb600fc26c2f9cceafbf0bc7b
ridhanmuhammad25-commits <ridhanmuhammad25@gmail.com>
2026-07-30 23:42:21 +0700

    a

--- YourMajor_Recomendation/app.py
+++ YourMajor_Recomendation/app.py
@@ -90,7 +90,7 @@ def inject_css():
         }
         
         .hero2 img {
-            width: 100%;
+            width: 50%;
             max-width: 1200px;
             height: 1000px;
             border-radius: 18px;
